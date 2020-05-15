bdd2630147aa8353c10f33a8d6522fe3cf251d84
pachr16 <32641694+pachr16@users.noreply.github.com>
2020-05-15 14:43:54 +0200

    references to the two different music and info servers now fixed

--- Microservices/Website/src/env_vars.js
+++ Microservices/Website/src/env_vars.js
@@ -1,7 +1,9 @@
 //for when runing servers locally
 export const LOGIN_SERVER = 'http://localhost:8080';
-export const MUSIC_SERVER = 'http://localhost:2000';
+export const MUSIC_SERVER = 'http://localhost:2001';
+export const INFO_SERVER = 'http://localhost:2000';
 
 //for useing containers
 // export const LOGIN_SERVER = 'https://192.168.99.100:443/login/';
 // export const MUSIC_SERVER = 'https://192.168.99.100:443/music/';
+// export const INFO_SERVER = 'INSERT DOCKER THINGY';
