75e2413ecd9be401cb716c031b06dd71e6449477
pachr16 <32641694+pachr16@users.noreply.github.com>
2020-05-15 14:08:43 +0200

    Initial setup done?

---
+++ Microservices/Userdata/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, email, password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        // more ?? playlists? artist verification?
+    }
+}
+
+module.exports = User;
